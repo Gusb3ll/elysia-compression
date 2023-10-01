7b22c9084999dff715fcf2cb0eb637059bb6faab
SaltyAom <saltyaom@gmail.com>
2023-10-01 18:29:39 +0700

    fix: content-type (#5)
    * :tada: feat: additional header
    
    * :tada: feat: content-type

--- example/index.ts
+++ example/index.ts
@@ -2,12 +2,22 @@ import { Elysia } from 'elysia'
 import { compression } from '../src/index'
 
 const app = new Elysia()
+<<<<<<< HEAD
   .use(compression())
   .all('/', () => ({ value: 'Hello, World!' }))
+  .all('/text', () => 'a')
   .listen(4000)
 
 console.log(
   `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
+=======
+    .use(compression())
+    .post('/', ({ set }) => ({ value: 'Hello, World!' }))
+    .listen(3000)
+
+console.log(
+    `Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+>>>>>>> 8837f0df944a1b7a5f6a109d18b2b0f3884821d3
 )
 
 export type App = typeof app
