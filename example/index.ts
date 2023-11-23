a0bf16566c3a229344b1e88cb5852791f03c5a15
Sebastian Kriems <sebastian.kriems@sailrs.io>
2023-11-23 19:53:30 +0100

    feat(stream): added SSE example

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,29 @@
 import { Elysia } from 'elysia'
 import { compression } from '../src/index'
+import Stream from '@elysiajs/stream'
 
 const app = new Elysia()
-  .use(compression({ type: 'deflate' }))
+  .use(compression({ type: 'gzip' }))
   .get('/', () => 'Hello, world!')
   .get('/json', () => ({ hello: 'world' }))
   .get('/pic', () => Bun.file('./tests/mei.jpg'))
+  .get(
+    '/sse',
+    () =>
+      new Stream(stream => {
+        let count = 0
+        const interval = setInterval(() => {
+          count++
+          stream.event = 'news'
+          stream.send(`event #${count}`)
+        }, 200)
+
+        setTimeout(() => {
+          clearInterval(interval)
+          stream.close()
+        }, 3000)
+      }),
+  )
   .listen(4000)
 
 console.log(
