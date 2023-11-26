48ab2e1538f9487e781d4cb0b649a35ba765d1e9
Sebastian Kriems <15573317+skriems@users.noreply.github.com>
2023-11-26 05:24:54 +0100

    feat: handle streaming responses (#7)
    * chore: bumped dependencies
    
    * feat(stream): polyfill for CompressionStream
    
    remove me once bun has implemented this
    
    * feat(stream): handle a streaming response
    
    * feat(stream): added SSE example

---
+++ src/stream.ts
@@ -0,0 +1,32 @@
+import zlib from 'node:zlib'
+
+/** NOTE: can be removed once Bun has implemented this class
+ *
+ * https://github.com/oven-sh/bun/issues/1723
+ * https://github.com/oven-sh/bun/issues/1723#issuecomment-1774174194
+ * */
+export class CompressionStream {
+  readable: ReadableStream
+  writable: WritableStream
+
+  constructor(format: 'gzip' | 'deflate') {
+    const handle =
+      format === 'deflate'
+        ? zlib.createDeflate()
+        : format === 'gzip'
+          ? zlib.createGzip()
+          : zlib.createDeflateRaw()
+
+    this.readable = new ReadableStream({
+      start(controller) {
+        handle.on('data', (chunk: Uint8Array) => controller.enqueue(chunk))
+        handle.once('end', () => controller.close())
+      },
+    })
+
+    this.writable = new WritableStream({
+      write: (chunk: Uint8Array) => handle.write(chunk),
+      close: () => handle.end(),
+    })
+  }
+}
